fbd59cf730563fa55a19e5c3e6e8163bfab7c0d8
svetl <svetlio.zhelev36@outlook.com>
2023-10-11 18:16:23 +0300

    Password Manager Generatior

--- pass_generator.py
+++ pass_generator.py
@@ -9,9 +9,14 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
 
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+# nr_letters = int(input("How many letters would you like in your password?\n"))
+# nr_symbols = int(input(f"How many symbols would you like?\n"))
+# nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
 
 # Eazy Level - Order not randomised:
 
